2084222b1136b706096672fee74d85b3d5dc7e69
youry07 <youry@ukr.net>
2025-08-28 21:36:52 +0300

    j03 in-progress

--- j03/main.py
+++ j03/main.py
@@ -1,87 +1,92 @@
-import pathlib
-from collections import deque
-
-from rich.console import Console
-from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import WordCompleter
-from prompt_toolkit.history import FileHistory
-
-from j01.classes import Service
-
-script_path = pathlib.Path(__file__)
-
-commands = {
-    "exit": 0
-}
-
-console = Console()
-
-history_path = script_path.with_name('.history')
-history = FileHistory(history_path)
-completer = WordCompleter(list(commands.keys()))
+import timeit
+import random
 
 
-session = PromptSession(
-        history=history, completer=completer, reserve_space_for_menu=True)
+def insertion_sort_my(lst):
+    sorted, unsorted = [], lst
+    while len(unsorted) > 0:
+        key = unsorted.pop(0)
+        for i, x in enumerate(sorted):
+            if key < x:
+                break
+        else:
+            i = len(sorted)
+        sorted.insert(i, key)
+    return sorted    
+    
+def insertion_sort_old(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i-1
+        while j >=0 and key < lst[j] :
+                lst[j+1] = lst[j]
+                j -= 1
+        lst[j+1] = key 
+    return lst
 
-service = Service()
+def merge_sort(lst):
+    if len(lst) < 2:
+        return lst
+    mid = len(lst) // 2
+    left_half = lst[:mid]
+    right_half = lst[mid:]
 
-def parse_input(msg_prompt: str):
-        msg: str = session.prompt(msg_prompt)
-        return msg.lower().strip()
+    return merge(merge_sort(left_half), merge_sort(right_half))
 
-def is_palindrome_simple(text: str):
-    text = text.replace(" ", "")
-    l = len(text)
-    part1 = text[:l//2]
-    if l % 2: 
-        part2 = text[l//2+1:] 
-    else:
-        part2 = text[l//2:]
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
 
-    part2 = part2[::-1]
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
 
-    #print(f"{part1} - {part2}")
-   
-    return part1 == part2
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
 
-def is_palindrome_dequeue(text: str):
-    text = text.replace(" ", "")
-    l = len(text)
-    part1 = text[:l//2]
-    if l % 2: 
-        part2 = text[l//2+1:] 
-    else:
-        part2 = text[l//2:]
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
 
-    dq2 = deque()
-    dq2.extendleft(part2)
-    dq1 = deque()
-    dq1.extend(part1)
+    return merged
+    
 
-    return list(dq1) == list(dq2)
-
+list_length = 100
+min_value, max_value = -100, 100
+random_lst = [random.randint(min_value, max_value) for _ in range(list_length)]
+number = 100000
 
 def main():
+
     
-    console.print("[bold green]Welcome to the Test a Palindrome![/bold green]")
-    console.print(
-        "Type text or 'exit' to quit.")
+    print(insertion_sort_my([random.randint(min_value, max_value) for _ in range(10)]))
+    print(insertion_sort_old([random.randint(min_value, max_value) for _ in range(10)]))
+    print(merge_sort([random.randint(min_value, max_value) for _ in range(10)]))
+    print(sorted([random.randint(min_value, max_value) for _ in range(10)]))
+        
+    code_insertion_sort_my = "insertion_sort_my(random_lst.copy())"
+    time_insertion_sort_my = timeit.timeit(code_insertion_sort_my, globals=globals(), number=number)
+    print(f"time_insertion_sort_my: {time_insertion_sort_my}")
+
+    code_insertion_sort_old = "insertion_sort_old(random_lst.copy())"
+    time_insertion_sort_old = timeit.timeit(code_insertion_sort_old, globals=globals(), number=number)
+    print(f"time_insertion_sort_old: {time_insertion_sort_old}")
+
+    code_merge_sort = "merge_sort(random_lst.copy())"
+    time_merge_sort = timeit.timeit(code_merge_sort, globals=globals(), number=number)
+    print(f"time_merge_sort: {time_merge_sort}")
+
+    code_sorted = "sorted(random_lst.copy())"
+    time_sorted = timeit.timeit(code_sorted, globals=globals(), number=number)
+    print(f"time_sorted: {time_sorted}")
     
-    while True:
-        repl = parse_input("Enter text: ")
-        try:
-            match repl:
-                case 'exit':
-                    console.print("[bold green]Good bye![/bold green]")
-                    break
-                case text:
-                    if is_palindrome_dequeue(text):
-                        console.print(f"[bold green]Yes[/bold green]")
-                    else:
-                        console.print(f"[bold red]No[/bold red]")
-        except Exception as ex:
-            console.print(f"[bold red]{ex}[/bold red]")
+    
 
 
     
